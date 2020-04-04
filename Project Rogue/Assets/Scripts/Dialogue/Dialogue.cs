bf3a478e700e47b70dce987959f59d77103124ff
VexedProgrammer <vexedprogrammer@gmail.com>
2020-04-04 04:07:33 +0100

    ADD: XML Dialogue system ADD: Dialogue trigger script ADD: Personal test scene

---
+++ Project Rogue/Assets/Scripts/Dialogue/Dialogue.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+//XML
+using System.Linq;
+using System.Xml.Linq;
+
+//UI
+using UnityEngine.UI;
+
+public class Dialogue : MonoBehaviour {
+
+    //XML
+    XDocument m_Doc; //Dialogue text
+    XElement m_Root; //Root node
+
+    //UI
+    [Header("UI")]
+    [SerializeField] private GameObject m_DiglauePopUp; //UI popup
+    [SerializeField] private Text m_DialgueText; //Text to display
+    [Space(0.1f)]
+    [SerializeField] private float m_fDisplaySpeed = 10; //Number fo characters to display per second
+
+    //Management
+    List<QueuedDialogue> m_QueuedDialogue = new List<QueuedDialogue>(); //List of dialogue to display
+
+    /// <summary>
+    /// Queued Dialogue struct
+    /// Holds infomation about the stored dialogue inluding how long it should be displayed for
+    /// </summary>
+    struct QueuedDialogue
+    {
+        public XName sceneName;
+        public uint id;
+        public float delay;
+
+        public QueuedDialogue(XName sceneName, uint id, float delay) //Constructor
+        {
+            this.sceneName = sceneName;
+            this.id = id;
+
+            this.delay = delay;
+        }
+    };
+
+    public delegate void Event(); //Dialogue event delegate (used for storing function pointer)
+    private Event m_Event;
+	// Use this for initialization
+	void Awake () {
+        TextAsset doc = Resources.Load("Dialogue") as TextAsset; //Load in text from xml file
+        if (!doc) { Debug.LogWarning("Dialgue: Start: Failed to load file."); return; } //Error check
+        Assert.IsTrue(doc.text.Length > 0);  //Make sure we have text not an empty or incorrectly loaded file
+        m_Doc = XDocument.Parse(doc.text); //Pass the text into a xml document format
+        m_Root = m_Doc.Root; //Get the root node
+
+    }
+	
+    public void Push(XName sceneName, uint id, float delay) //Push dialogue to the list
+    {
+        m_QueuedDialogue.Add(new QueuedDialogue(sceneName, id, delay));
+    }
+
+    public void Flush(bool close) //Flush the current list and show all dialogue 
+    {
+        StartCoroutine(FlushDialogue(null, close));
+    }
+
+    public void Flush(Event trigger, bool close) //Flush but call a function once it is compelte
+    {
+        StartCoroutine(FlushDialogue(trigger, close));
+    }
+
+    private IEnumerator FlushDialogue(Event trigger, bool close)
+    {
+        m_DiglauePopUp.SetActive(true); //Display dialogue popup
+        while (m_QueuedDialogue.Count() > 0) //Loop through the list
+        {
+            QueuedDialogue queuedDialogue = m_QueuedDialogue[0]; //Get dirst element
+            yield return DisplayText(queuedDialogue.sceneName, queuedDialogue.id); //Call display
+            m_QueuedDialogue.RemoveAt(0); //Remove first element
+            yield return new WaitForSeconds(queuedDialogue.delay); //Wait
+        }
+        m_DiglauePopUp.SetActive(!close); //Close the dialogue box if it close is true
+        if (trigger != null) //If there is a function event call it
+            trigger();
+    }
+
+    private IEnumerator DisplayText(XName sceneName, uint id)
+    {
+        string text = ""; //Set empty string
+        XElement scene = m_Root.Element(sceneName); //Get scene node
+        Assert.IsTrue(scene != null);
+        foreach (XElement ele in scene.Elements()) //Loop through and find correct dialogue based on id
+        {
+            if (ele.FirstAttribute.Value == id.ToString())
+            {
+                text = ele.Value;
+            }
+        }
+
+        if (text.Length <= 0) //check if the dialogue exsists
+        {
+            Debug.LogWarning(string.Format("Dialgue: getDialogue: Failed to find element in scene {0} with id {1}", sceneName, id));
+            yield break;
+        }
+
+        WaitForSeconds wait = new WaitForSeconds(1.0f / m_fDisplaySpeed); //Set up wait
+
+        text = text.Replace("\\n", "\n"); //Replace default new line character with one recognised by unity
+        m_DialgueText.text = ""; //Set dialogue text to be empty
+
+        foreach(char c in text) //loop through each character and print them out
+        {
+            m_DialgueText.text += c;
+            yield return wait;
+        }
+    }
+}
---
+++ Project Rogue/Assets/Scripts/Dialogue/TriggerDialogue.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class TriggerDialogue : MonoBehaviour {
+
+    [SerializeField] private Dialogue m_Dialogue;
+    [SerializeField] private string m_sSceneName = "Test";
+    [SerializeField] private uint m_iID = 1;
+    [SerializeField] private float m_fDisplayTime = 2.0f;
+    private bool m_bTriggered = false;
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (m_bTriggered) return;
+        if (other.tag == "Player")
+        {
+            m_bTriggered = true;
+            m_Dialogue.Push(m_sSceneName, m_iID, m_fDisplayTime);
+            m_Dialogue.Flush(true);
+        }
+    }
+
+    private void Start()
+    {
+        m_Dialogue.Push(m_sSceneName, m_iID, m_fDisplayTime);
+        m_Dialogue.Flush(true);
+    }
+}
