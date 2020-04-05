ef9a6c522920e2833e3ad480bbe872ddc6727762
Marcus <marcushvingelby@gmail.com>
2020-04-05 20:36:56 +0200

    Automatic pickup system
    ADD: Automatic pickup system (WIP)

---
+++ Project Rogue/Assets/Scripts/EnviromentalInteractions.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnviromentalInteractions : MonoBehaviour
+{
+    public void OnInteracted()
+    {
+        Destroy(gameObject);
+        // Add the item to the player's inventory
+        print("Enviromental Interactions: Automatic Item Pickup: \n " + gameObject.name + " has been picked up and added to the player's inventory.");
+        print("Failed to add object: " + gameObject.name + " to the player's inventory, error: \n No inventory has been asigned in 'EnviromentalInteractions.cs'!");
+    }
+}
---
+++ Project Rogue/Assets/Scripts/PickupSystem.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupSystem : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("AutoPickup"))
+        {
+            other.GetComponent<EnviromentalInteractions>().OnInteracted();
+        }
+    }
+}
